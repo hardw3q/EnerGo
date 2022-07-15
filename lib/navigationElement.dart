9144faca82e5511c25c1c1f3fd3a5679d6d2e9a4
Andrey Pankov <37929122+hardw3q@users.noreply.github.com>
2022-07-15 22:10:50 +0300

    Added BottomNavigation

---
+++ lib/navigationElement.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class NavigationElement extends StatelessWidget {
+  const NavigationElement({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
